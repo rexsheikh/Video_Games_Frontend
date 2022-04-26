aa00091e1da22d71f71e0cefb438f7f5fc1127ff
unknown <rex.sheikh@gmail.com>
2022-04-26 14:13:51 -0700

    modified the get console sales async function to properly deliver all necessary data.

--- src/Components/SalesByConsole.jsx
+++ src/Components/SalesByConsole.jsx
@@ -5,8 +5,7 @@ import { Chart } from "react-google-charts"
 const SalesByConsole = () => {
 
     const [consoles,setConsoles] = useState([]);
-    let salesByConsole = [];
-    let consoleCount = consoles.length
+    const [salesByConsole,setSalesByConsole] = useState([]);
 
 
     useEffect(() => {
@@ -20,66 +19,39 @@ const SalesByConsole = () => {
                 "https://localhost:7260/api/games/"
             );
             setConsoles(result.data)
-            console.log(`CONSOLES : ${consoles}`)
         } catch (e) {
             console.log(e.message);
         }
     }
 
     const getSalesByConsole = async() => {
+        let container = [];
         try { 
-            for(let i = 0; i < consoleCount; i++){
+            for(let i = 0; i < consoles.length; i++){
                 let result = await axios.get(
                     `https://localhost:7260/api/games/${consoles[i]}`
                 )
-            salesByConsole.push(result.data)
+            container.push(result.data)
             }
+            setSalesByConsole(container)
         } catch (e) {
             console.log(e.message)
         }
-        console.log(salesByConsole)
     }
-        //[platform,sales]
-        function packageData(plat,sales){
-            let res = []
-            for(let i = 0; i < consoleCount;i++){
-                res.push([plat[i],sales[i]]);
-            }
-            return res
-        }
-
-        let sales = packageData(consoles,salesByConsole);
-        console.log(`SALES: ${sales}`)
-    // export const data = [
-    //     ["Element", "Density", { role: "style" }],
-    //     ["Copper", 8.94, "#b87333"], // RGB value
-    //     ["Silver", 10.49, "silver"], // English color name
-    //     ["Gold", 19.3, "gold"],
-    //     ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
-    //   ];
-      
-    //   export function App() {
-    //     return (
-    //       <Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
-    //     );
-    //   }
-
-        const data = [
-            ["Platform","TotalSales",{ role: "style" }],
-            [sales,"silver"]
-        ]
+    
+    
+    console.log(`Sales by console: ${salesByConsole}`)
+    let data = [[...consoles],[...salesByConsole]]
+    console.log(`data: ${data}`)
       
     return (  
         <div>
-            <Chart
+            {/* <Chart
                 chartType = 'ColumnChart'
                 width = "100%"
                 height = "400px"
                 data = {data}
-                data = {[["Consoles", "Sales By Console"], ...consoles, salesByConsole]}
-                options = {{legend: {position: 'bottom'}}}
-                legendToggle
-            />
+            /> */}
         </div> 
     );
 }
