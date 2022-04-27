0f53d60207a971f729b5122a2eeeeffb87609079
unknown <rex.sheikh@gmail.com>
2022-04-27 12:51:09 -0700

    Refactored the salesByConsole to properly package the data in a format that is accepted by the google charts.

--- src/Components/SalesByConsole.jsx
+++ src/Components/SalesByConsole.jsx
@@ -3,53 +3,45 @@ import axios from 'axios';
 import { Chart } from "react-google-charts"
 
 const SalesByConsole = () => {
-
-    const [consoles,setConsoles] = useState([]);
     const [salesByConsole,setSalesByConsole] = useState([]);
-
-    function buildSalesByConsole(){
-        getConsoles()
-            .then(getSalesByConsole)
-    }
-
-
-
+    // const [chartData, setChartData] = useState([]);
 
     useEffect(() => {
-        buildSalesByConsole();
+        getSalesByConsole();
     },[])
 
-    function getConsoles(){
+    const getSalesByConsole = async () => {
         try {
-            let result = axios.get(
+            let result = await axios.get(
                 "https://localhost:7260/api/games/"
             );
-            setConsoles(result.data)
+            setSalesByConsole(result.data)
         } catch (e) {
             console.log(e.message);
         }
     }
+    // useEffect(() => {
+    //     let tempChartData = salesByConsole.map(entry => {
+    //         return [entry.consoles, entry.sales];
+    //     });
+    //     setChartData(tempChartData);
+    // }, [salesByConsole])
 
-    function getSalesByConsole() {
+    function packageData(obj){
         let container = [];
-        try { 
-            for(let i = 0; i < consoles.length; i++){
-                let result = axios.get(
-                    `https://localhost:7260/api/games/${consoles[i]}`
-                )
-            container.push(result.data)
-            }
-            setSalesByConsole(container)
-        } catch (e) {
-            console.log(e.message)
-        }
+        container.push(Array("Platform", "Sales", { role: "style" }))
+        for(let[key,value] of Object.entries(obj)){
+            container.push(Array(key,value,"blue"))
+            console.log(`${key}: ${value}`)
+            console.log(`container: ${container}`)
     }
+    return container
+}
+
+let data = packageData(salesByConsole)
+
+
     
-    
-    
-    let data = [["Platform",...consoles],["Sales",...salesByConsole]]
-    console.log(`Data: ${data}`)
-      
     return (  
         <div>
             <Chart
