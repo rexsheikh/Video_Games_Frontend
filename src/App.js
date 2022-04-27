ad29f7e48812164693b0ece6a20311227b94e0ae
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-04-27 08:27:04 -0400

    Created a search games component that uses a rudimentary filter to match a search input with the games in the database. Created a display games function that takes in that search ad displays results in a table.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 import SalesByConsole from "./Components/SalesByConsole";
 
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+
+const [games,setGames] = useState([]); 
+const [allGames,setAllGames] = useState([]);
+
+useEffect(() => {
+        getAllGames();
+    },[])
+
+
+function newSearch(search){   {/*this takes in the search as a string*/}
+    let newGames = games.filter(function(el){
+    if(el.name.includes(search)){
+        return true;
+      }
+    })
+    setGames(newGames);
+  }
+
+
+  const getAllGames = async() => {
+      try { 
+              let result = await axios.get(
+                  `https://localhost:7260/api/games/names/`
+              )
+          setAllGames(results.data)
+      } catch (e) {
+          console.log(e.message)
+      }
+  }
+
 function App() {
   return (
     <div >
---
+++ src/Components/DisplayGames.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const GameTable = (props) => {
+
+    return ( 
+    <div>
+        <thead>
+            <tr> 
+                <th className="text-center font-weight-bold"> </th>
+                <th className="text-center font-weight-bold"> </th>
+                <th className="text-center font-weight-bold"> </th>
+                <th className="text-center font-weight-bold"> </th>
+                <th className="text-center font-weight-bold"> </th>
+            </tr>
+        </thead>
+        <tbody>
+        {props.parentGames.map((game) =>{
+            return (
+                <tr> 
+                    <td className="text-center"> {game.name} </td>
+                    {/* look up column data and map appropriately. */}
+                </tr>
+            )
+        })}
+        </tbody>
+    </div>
+    );
+}
+ 
+export default MusicTable;
