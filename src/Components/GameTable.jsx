0c443dd1e198f8400f12974ec118c8ea9400766a
unknown <rex.sheikh@gmail.com>
2022-04-30 11:35:25 -0700

    refactored the games component to show specific games' sales by console data.

--- src/Components/GameTable.jsx
+++ src/Components/GameTable.jsx
@@ -1,70 +1,71 @@
-import { Chart } from "react-google-charts";
-import './GameTable.css';
+import { useState } from "react";
+import { Chart } from "react-google-charts"
+import Table from 'react-bootstrap/Table'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const GameTable = (props) => {
-    // let example = props.parentGames[0].name;
-    // console.log(`EXAMPLE: ${example}`)
-    // // export const data = [
-    // //     ["Element", "Density", { role: "style" }],
-    // //     ["Copper", 8.94, "#b87333"], // RGB value
-    // //     ["Silver", 10.49, "silver"], // English color name
-    // //     ["Gold", 19.3, "gold"],
-    // //     ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
-    // //   ];
 
-    function packageData(obj){
-        let container = [];
-        container.push(Array("Platform", "Sales", { role: "style" }))
+    function matchesToData(obj){
+        let dict = {};
+        let matches;
         for(let i = 0; i < obj.length; i++){
-            container.push(Array(props.parentGames[i].platform,props.parentGames[i].globalSales,"#483D8B"))
+            matches = obj.filter(item => item.name.indexOf(obj[i].name) !== -1);
+            dict[obj[i].name] = generateChartData(matches)
+        }
+        return dict
+    }
+
+    function generateChartData(matches){
+        let container = [];
+        container.push((Array("Platform", "Sales", { role: "style" })));
+        for(let i = 0; i < matches.length; i++){
+            container.push(Array(matches[i].platform,matches[i].globalSales,"blue"))
         }
         return container
     }
-    
-    let data = packageData(props.parentGames)
-    console.log(`CONTAINER DATA: ${data}`)
-    let options = {
-        backgroundColor: '#B9B2E2'
-    }
-    return ( 
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <th className="searchTable">
-                    <th className="text-center font-weight-bold"> Name</th>
-                    </th> 
-                    <th className="searchTable">
-                    <th className="text-center font-weight-bold"> Platform</th>
-                    </th>
-                    <th className="searchTable">
-                    <th className="text-center font-weight-bold"> Year </th>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-            {props.parentGames.map((game,index) =>{
-                return (
-                    <tr key = {index}> 
-                        <td className="text-center"> {game.name} </td>
-                        <td className="text-center"> {game.platform} </td>
-                        <td className="text-center"> {game.year} </td>
-                    </tr>
-                )
-            })}
-            </tbody>
-        </table>   
+
+
+
+
+    if(props.parentGames.length === 0){
+        return(
+            <div> 
+                <p>Enter a search term....</p>
+            </div>
+        )
+    }else{
+        let dict = matchesToData(props.parentGames)
+        return ( 
         <div>
-            <Chart
-                chartType = 'ColumnChart'
-                width = "75%"
-                height = "100px"
-                data = {data}
-                options = {options}
-            />
-        </div> 
-    </div>
-    );
+            <table className="table table-dark">
+                <thead>
+                    <tr> 
+                        <th className="text-center font-weight-bold"> Name</th>
+                        <th className="text-center font-weight-bold"> Platform</th>
+                        <th className="text-center font-weight-bold"> Sales per Console </th>
+                    </tr>
+                </thead>
+                <tbody>
+                {props.parentGames.map((game,index) =>{
+                    return (
+                        <tr key = {index}> 
+                            <td className="text-center"> {game.name} </td>
+                            <td className="text-center"> {game.platform} </td>
+                            <th className="zoom"><Chart
+                                chartType = 'ColumnChart'
+                                width = "100%"
+                                height = "100%"
+                                data = {dict[game.name]} 
+                    />
+                      </th>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
+        );
+            }
 }
  
 export default GameTable;
